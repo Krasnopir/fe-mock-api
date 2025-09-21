3a6de4619fbd2e07e9663afdece59bb5e9be9f57
Krasnopir Petro <p.krasnopir@gmail.com>
2025-09-21 17:41:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { faker } from "@faker-js/faker";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 3000;
 
 const AUTH_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImQ2M2QyYjNmOTE0NjdlZDE4MTBlYmZlZjBlZmJhMjFjIn0.eyJpZCI6ImQ5MjI0MGUzLTc4OWItNGM4NS05OWVkLTY4N2Q2NWRmN2RmZCJ9.2AWJAapQ3jheVjyNIFQvTHg9QytCgzzY4JkcirzmDAMMsEKmXcnyE2nSVYySez3Qxh3bahoJLKKM_zT3WJummg";
